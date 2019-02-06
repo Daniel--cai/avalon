52aea1c30398285d01acd5468285e70c4161c05a
Daniel <daniel.cai92@gmail.com>
2019-02-06 23:37:40 +1100

    Add ingress and controller for api and websocket

--- Avalon.Server/go.mod
+++ Avalon.Server/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.3.0
 	github.com/go-siris/siris v7.4.0+incompatible // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.0
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.2.8 // indirect
--- Avalon.Server/main.go
+++ Avalon.Server/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 type Article struct {
@@ -26,6 +30,12 @@ type Request struct {
 
 type Response struct {
 	Data string `json:"body"`
+}
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func getClient() string {
@@ -73,12 +83,31 @@ func allArticles(context *gin.Context) {
 
 func homePage(context *gin.Context) {
 	context.JSON(200, gin.H{
-		"message": "Hello Worlsddsssd",
+		"message": "Hello Worlsdddsssd",
 	})
+}
+
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsupgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Failed to set websocket upgrade: %+v", err)
+		return
+	}
+
+	for {
+		t, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		conn.WriteMessage(t, msg)
+	}
 }
 
 func main() {
 	r := gin.Default()
+	r.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
 	r.GET("/", homePage)
 	r.GET("/articles", allArticles)
 	r.Run(":8081")
