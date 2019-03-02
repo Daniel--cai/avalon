2f934ffadc08b1189a390f41718cd1363ccdca65
Daniel <daniel.cai92@gmail.com>
2019-03-02 23:26:33 +1100

    Clean up lobby lambda

--- Avalon.Serverless/src/lib/lambda/lobby-lambda.ts
+++ Avalon.Serverless/src/lib/lambda/lobby-lambda.ts
@@ -35,3 +35,39 @@ export const getPlayers: Handler = async (event, context) => {
   };
   return success;
 };
+
+export const createLobby: Handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event,
+      context: context
+    })
+  };
+
+  return response;
+};
+
+export const GetLobby: Handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event,
+      context: context
+    })
+  };
+
+  return response;
+};
+
+export const connectionLobby: Handler = async (event, context) => {
+  console.log("connected");
+  if (event.requestContext.eventType === "CONNECT") {
+    console.log("conection connected!");
+  } else if (event.requestContext.eventType === "DISCONNECT") {
+    console.log("conection disconnected!");
+  }
+  return success;
+};
